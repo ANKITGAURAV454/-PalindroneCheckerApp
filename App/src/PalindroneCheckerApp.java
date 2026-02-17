894012340dd7c7064744aa9b337a71904b6fcfdc
Ankit Gaurav <ankitgaurav84@gmail.com>
2026-02-17 15:42:56 +0530

    [Ankit] Completed UC1

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,6 +1,8 @@
 public class PalindroneCheckerApp {
     public static void main(String[] args){
         System.out.println("Welcome to Palindrone Checker App");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized Successfully");
     }
 
 }
