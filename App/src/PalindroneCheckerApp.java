248842077f0c2ff516dfd1ee5bae70914a48c768
Ankit Gaurav <ankitgaurav84@gmail.com>
2026-02-17 15:32:43 +0530

    [Ankit] Solved UC1

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,6 +1,10 @@
+import java.awt.*;
+
 public class PalindroneCheckerApp {
     public static void main(String[] args){
-        System.out.println("Welcome to Palindrone Checker App");
+        System.out.println("Welcome to Palindrone Checker Management System");
+        System.out.println("Version :1.0");
+        System.out.println("System Initilized verion");
     }
 
 }
