37129f575e3a39b4e064b35a7bb74d1c7fecc10e
Ankit Gaurav <ankitgaurav84@gmail.com>
2026-02-17 15:01:57 +0530

    [Ankit] solved UC1

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class PalindroneCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrone Checker App");
+    }
+
 }
