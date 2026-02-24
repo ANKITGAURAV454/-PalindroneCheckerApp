3ac9e643c31eae5805ab1e8a4f2489399413e3d9
ANKITGAURAV454 <ankitgaurav84@gmail.com>
2026-02-24 14:47:52 +0530

    [Ankit] complete UC4

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -2,23 +2,33 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
+
         String palindrome;
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Input Text: ");
         palindrome = sc.nextLine();
-        String rev_palindrome;
-        int count = 0;
-        for (int i = 0; i < palindrome.length(); i++) {
-            if (palindrome.charAt(i) == palindrome.charAt(palindrome.length() - i - 1)) {
-                count++;
+
+        // Convert string to character array
+        char[] characters = palindrome.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean is_palindrome = true;
+
+        // Two-pointer technique
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                is_palindrome = false;
+                break;   // stop immediately if mismatch found
             }
+            start++;
+            end--;
         }
 
-        boolean is_palindrome = false;
-        if (count == palindrome.length()) {
-            is_palindrome = true;
-        }
-        System.out.println("Is it a Palindrome? : "+is_palindrome);
+        System.out.println("Is it a Palindrome? : " + is_palindrome);
 
+        sc.close();
     }
 }
